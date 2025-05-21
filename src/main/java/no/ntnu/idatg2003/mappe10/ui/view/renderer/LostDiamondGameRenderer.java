329f1f90bce9c79d1881ec1573bdbfeb77a3515f
Stian Julius Vo <stiajv@stud.ntnu.no>
2025-05-22 01:51:05 +0200

    Board tiles and player piece drawn to board.

--- src/main/java/no/ntnu/idatg2003/mappe10/ui/view/renderer/LostDiamondGameRenderer.java
+++ src/main/java/no/ntnu/idatg2003/mappe10/ui/view/renderer/LostDiamondGameRenderer.java
@@ -3,11 +3,18 @@ package no.ntnu.idatg2003.mappe10.ui.view.renderer;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import no.ntnu.idatg2003.mappe10.model.coordinate.Coordinate;
 import no.ntnu.idatg2003.mappe10.ui.controller.BoardGameController;
 
 import java.io.InputStream;
 
 public class LostDiamondGameRenderer extends Renderer {
+
+  private final GraphicsContext gc = super.getCanvas().getGraphicsContext2D();
+
+  private final double diameter = 30;
+  private final double radius = diameter / 2;
 
   public LostDiamondGameRenderer(BoardGameController controller, Canvas canvas) {
     super(controller, canvas);
@@ -16,17 +23,61 @@ public class LostDiamondGameRenderer extends Renderer {
   @Override
   public void drawBoard() {
     drawBackground();
-    super.drawTiles();
-    super.numerateTiles();
-    super.drawPlayers(1.4);
+
+    drawTiles();
+
+    drawPlayers();
   }
 
   @Override
   public void drawBackground() {
-    GraphicsContext gc = super.getCanvas().getGraphicsContext2D();
     InputStream io = getClass().getResourceAsStream("/board/lostDiamondBackground.png");
-    Image background = new Image(io);
-    gc.drawImage(background,0,0, getCanvasWidth(), getCanvasHeight());
+    Image background = new Image(io, getCanvasWidth(), getCanvasHeight(), true, true);
+    gc.drawImage(background, 0, 0, getCanvasWidth(), getCanvasHeight());
+  }
+
+  @Override
+  public void drawTiles() {
+    int numberOfTiles = super.getController().getNumberOfTiles();
+    for (int i = 1; i <= numberOfTiles; i++) {
+      Coordinate canvasCoords = super.getController().getCanvasCoords(i, getOffsetWidth(), getOffsetHeight());
+      double x = canvasCoords.getX0();
+      double y = canvasCoords.getX1();
+
+      gc.setFill(Color.YELLOW);
+
+      gc.fillOval(x - radius, y - radius, diameter, diameter);
+    }
+  }
+
+  @Override
+  public void drawPlayers() {
+    super.getController().getPlayerListIterator().forEachRemaining(player -> {
+      InputStream inputStream = getClass().getResourceAsStream("/playingPieces/" + player.getPlayingPiece() + ".png");
+      if (inputStream == null) {
+        System.out.println("Image not found: " + player.getPlayingPiece());
+        return;
+      }
+
+      Image image = new Image(inputStream);
+      double scale = 0.67 * Math.min(diameter / image.getWidth(), diameter / image.getHeight());
+      double scaledWidth = image.getWidth() * scale;
+      double scaledHeight = image.getHeight() * scale;
+
+      int currentTileID = player.getCurrentTile().getTileId();
+      Coordinate canvasCoords = super.getController()
+            .getCanvasCoords(currentTileID, super.getOffsetWidth(), super.getOffsetHeight());
+      double x = canvasCoords.getX0();
+      double y = canvasCoords.getX1();
+
+      double centeredX = x - scaledWidth / 2;
+      double centeredY = y - scaledHeight / 2;
+
+      gc.setFill(Color.YELLOW);
+      gc.fillOval(x - radius, y - radius, diameter, diameter);
+
+      gc.drawImage(image, centeredX, centeredY, scaledWidth, scaledHeight);
+    });
   }
 
 }
